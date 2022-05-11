80738078fd991e8c63b7005955cfb09c569bc39d
yusufnoor786 <zuzu950444@gamil.com>
2022-05-11 10:34:03 +0000

    Soong: Add Banner & Altho Codename to dumpvars.go

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -137,9 +137,11 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
+	"CUSTOM_VERSION"
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
-	"CUSTOM_VERSION",
+	"ALTHO_CODENAME"
+	"ALTHO_MAINTAINER"
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_BUILD_TYPE",
@@ -170,7 +172,12 @@ var BannerVars = []string{
 func Banner(make_vars map[string]string) string {
 	b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
+	fmt.Fprintln(b, "=================================================")
+	fmt.Fprintln(b, "                                                 ")
+	fmt.Fprintln(b, "     Welcome to Project Altho Build Environment  ")
+	fmt.Fprintln(b, "                       By                        ")
+	fmt.Fprintln(b, "                   Zabuka ZuZu                   ")
+	fmt.Fprintln(b, "=================================================")
 	for _, name := range BannerVars {
 		if make_vars[name] != "" {
 			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
